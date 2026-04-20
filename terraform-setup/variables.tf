713f0b69c5da58ed5b07b6ef8f475d7ec8aa6633
Kheav Kienghok <156294487+Kheav-Kienghok@users.noreply.github.com>
2026-04-20 20:06:15 +0700

    Update variables.tf

--- terraform-setup/variables.tf
+++ terraform-setup/variables.tf
@@ -1,22 +1,25 @@
 variable "aws_region" {
   description = "AWS region for the EC2 instances"
   type        = string
-  default     = "ap-southeast-1"
+  default     = "us-east-1"
 }
 
 variable "key_name" {
   description = "Name for the AWS key pair to create"
   type        = string
+  default     = "devop-final-key"
 }
 
 variable "ami_id" {
   description = "AMI ID to use for EC2 instances (set an AMI allowed by your organization policies)"
   type        = string
+  default     = "ami-009d9173b44d0482b"
 }
 
 variable "my_ip_cidr" {
   description = "Your public IP in CIDR format, used for SSH access"
   type        = string
+  default     = "0.0.0.0/0"
 }
 
 variable "ssh_user" {
